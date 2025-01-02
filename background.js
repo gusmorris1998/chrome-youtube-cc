e9abe81e5d5a595cff619b92ae4072a2b6cbc78f
gusmorris1998 <gusmorris1998@gmail.com>
2025-01-02 20:17:49 +1100

    Attempting to re-direct request, but problem with manifest v3 in Chrome

--- background.js
+++ background.js
@@ -1,14 +1,5 @@
-console.log("Service worker started");
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    console.log("onUpdated triggered");
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-        const queryParameters = tab.url.split("?")[1];
-        const urlParameters = new URLSearchParams(queryParameters);
-        console.log(urlParameters);
-
-        chrome.tabs.sendMessage(tabId, {
-            type: "NEW",
-            videoId: urlParameters.get("v"),
-        });
-    }
-});
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.declarativeNetRequest.getDynamicRules((rules) => {
+      console.log("Dynamic Rules:", rules);
+    });
+  });
